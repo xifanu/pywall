c57fe0ed2bd377b4fd969f61546428956f9760ee
xifanu <31371729+xifanu@users.noreply.github.com>
2024-08-28 17:12:13 +0800

    Update v6ipt.sh
    upgrade

--- v6ipt.sh
+++ v6ipt.sh
@@ -8,12 +8,8 @@ source '/etc/os-release'
 VERSION=$(echo "${VERSION}" | awk -F "[()]" '{print $2}')
 
 ip6tables -F
-ip6tables -X
+# ip6tables -X
 ip6tables -Z
-ip6tables -P INPUT ACCEPT
-ip6tables -P OUTPUT ACCEPT
-ip6tables -P FORWARD ACCEPT
-
 ip6tables -I INPUT -p icmpv6 --icmpv6-type router-solicitation -m hl --hl-eq 255 -j ACCEPT
 ip6tables -I INPUT -p icmpv6 --icmpv6-type router-advertisement -m hl --hl-eq 255 -j ACCEPT
 ip6tables -I INPUT -p icmpv6 --icmpv6-type neighbor-solicitation -m hl --hl-eq 255 -j ACCEPT
@@ -29,28 +25,36 @@ ip6tables -A INPUT -p udp --dport 8000:9999 -j ACCEPT
 ip6tables -A INPUT -p tcp --dport 44422 -j ACCEPT
 ip6tables -A INPUT -i lo -j ACCEPT
 ip6tables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+ip6tables -A INPUT -s ::/128  -j ACCEPT
+ip6tables -A INPUT -s ::1/128  -j ACCEPT
+ip6tables -A INPUT -s ::ffff:0:0/96  -j ACCEPT
+ip6tables -A INPUT -s 100::/64  -j ACCEPT
+ip6tables -A INPUT -s 64:ff9b::/96  -j ACCEPT
+ip6tables -A INPUT -s 2001::/32  -j ACCEPT
+ip6tables -A INPUT -s 2001:10::/28  -j ACCEPT
+ip6tables -A INPUT -s 2001:20::/28  -j ACCEPT
+ip6tables -A INPUT -s 2001:db8::/32  -j ACCEPT
+ip6tables -A INPUT -s 2002::/16  -j ACCEPT
+ip6tables -A INPUT -s fc00::/7  -j ACCEPT
+ip6tables -A INPUT -s fe80::/10  -j ACCEPT
+ip6tables -A INPUT -s ff00::/8  -j ACCEPT
 ip6tables -A INPUT -s 2a09:bac5:636e:1173::1bd:a0 -j ACCEPT
+ip6tables -A INPUT -s 2a02:7080::85b9:3a7d:a07d:1 -j ACCEPT
+
 # 禁止其他不匹配的规则访问本机
 ip6tables -P INPUT DROP
-#ip6tables -A INPUT -j DROP
-#配置写入文件
 
+# 配置写入文件
 if [[ "${ID}" == "centos" && ${VERSION_ID} -ge 7 ]]; then
     echo -e "${OK} ${GreenBG} 当前系统为 Centos ${VERSION_ID} ${VERSION} ${Font}"
     service ip6tables save
     systemctl restart ip6tables
 elif [[ "${ID}" == "debian" && ${VERSION_ID} -ge 8 ]]; then
     echo -e "${OK} ${GreenBG} 当前系统为 Debian ${VERSION_ID} ${VERSION} ${Font}"
-    modprobe ip_tables
     ip6tables-save > /etc/iptables/rules.v6
-    netfilter-persistent save
-    netfilter-persistent reload
 elif [[ "${ID}" == "ubuntu" && $(echo "${VERSION_ID}" | cut -d '.' -f1) -ge 16 ]]; then
     echo -e "${OK} ${GreenBG} 当前系统为 Ubuntu ${VERSION_ID} ${UBUNTU_CODENAME} ${Font}"
-    modprobe ip_tables
     ip6tables-save > /etc/iptables/rules.v6
-    netfilter-persistent save
-    netfilter-persistent reload
 else
     echo -e "${Error} ${RedBG} 当前系统为 ${ID} ${VERSION_ID} 不在支持的系统列表内，安装中断 ${Font}"
     exit 1
